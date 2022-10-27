803271a81fb6abd0b586bda32aea262257c94a3f
Aamir <aamir.bashir@codebrewinnovations.com>
2022-10-27 09:41:18 +0530

    grid view

--- build/generated/ap_generated_sources/debug/out/com/app/unl_map_sdk/BR.java
+++
@@ -1,5 +0,0 @@
-package com.app.unl_map_sdk;
-
-public class BR {
-  public static int _all = 0;
-}
--- build/generated/data_binding_trigger/debug/com/app/unl_map_sdk/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.app.unl_map_sdk;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
