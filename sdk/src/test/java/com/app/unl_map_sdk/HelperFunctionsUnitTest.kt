ffeb3f4a077d1a0cf18b7c23e4a70ff4314440e6
Aamir <aamir.bashir@codebrewinnovations.com>
2022-12-09 10:59:46 +0530

    some changes

--- sdk/src/test/java/com/app/unl_map_sdk/HelperFunctionsUnitTest.kt
+++ sdk/src/test/java/com/app/unl_map_sdk/HelperFunctionsUnitTest.kt
@@ -22,9 +22,10 @@ class HelperFunctionsUnitTest {
     @Test
     fun locationIdLatLng_isCorrect() {
         var expectedData = LatLng(47.17500002, 21.73100011)
-        var locationId="u2rkqebn312h"
+        var locationId = "u2rkqebn312h"
         assertEquals(expectedData, locationIdToLngLat(locationId))
     }
+
     /**
      * [getGridCell_isCorrect] method is a Test Case to Test [getCell] method.
      *  And expected return data is [GridCell] according to LocationId and Cell Size.
@@ -32,11 +33,12 @@ class HelperFunctionsUnitTest {
     @Test
     fun getGridCell_isCorrect() {
         var latLn = LatLng(47.17500002, 21.73100011)
-        var locationId="u2rkqebn3"
-        var cellSize="4.8m x 4.8m"
-        var expectedData = GridCell(locationId,cellSize )
+        var locationId = "u2rkqebn3"
+        var cellSize = "4.8m x 4.8m"
+        var expectedData = GridCell(locationId, cellSize)
         assertEquals(expectedData, getCell(latLn, CellPrecision.GEOHASH_LENGTH_9))
     }
+
     /**
      * [locationIdToBoundsCoordinates_isCorrect] method is a Test Case to Test [locationIdToBoundsCoordinates] method.
      *  And expected return data is ArrayList<List<Point>> for Selected LocationId.
@@ -45,14 +47,14 @@ class HelperFunctionsUnitTest {
     fun locationIdToBoundsCoordinates_isCorrect() {
         val expectedData: ArrayList<List<Point>> = ArrayList()
         val points: ArrayList<Point> = ArrayList()
-        var locationId="u2rkqebn312h"
+        var locationId = "u2rkqebn312h"
         points.add(Point.fromLngLat(21.73099994659424, 47.175000105053186))
         points.add(Point.fromLngLat(21.73099994659424, 47.17499993741512))
         points.add(Point.fromLngLat(21.731000281870365, 47.17499993741512))
         points.add(Point.fromLngLat(21.731000281870365, 47.175000105053186))
         points.add(Point.fromLngLat(21.73099994659424, 47.175000105053186))
         expectedData.add(points)
-        assertEquals(expectedData,locationIdToBoundsCoordinates(locationId))
+        assertEquals(expectedData, locationIdToBoundsCoordinates(locationId))
     }
 
     /**
@@ -61,16 +63,17 @@ class HelperFunctionsUnitTest {
      */
     @Test
     fun getFormattedCellDimensions_isCorrect() {
-        var expectedData="4.8m x 4.8m"
-        assertEquals(expectedData,getFormattedCellDimensions(CellPrecision.GEOHASH_LENGTH_9))
+        var expectedData = "4.8m x 4.8m"
+        assertEquals(expectedData, getFormattedCellDimensions(CellPrecision.GEOHASH_LENGTH_9))
     }
+
     /**
      * [getMinGridZoom_isCorrect] method is a Test Case to Test [getMinGridZoom] method.
      *  And expected return data is [ZoomLevel] enum for Selected [CellPrecision]
      */
     @Test
     fun getMinGridZoom_isCorrect() {
-        var expectedData= ZoomLevel.MIN_GRID_ZOOM_GEOHASH_LENGTH_9
-        assertEquals(expectedData,getMinGridZoom(CellPrecision.GEOHASH_LENGTH_9))
+        var expectedData = ZoomLevel.MIN_GRID_ZOOM_GEOHASH_LENGTH_9
+        assertEquals(expectedData, getMinGridZoom(CellPrecision.GEOHASH_LENGTH_9))
     }
 }
